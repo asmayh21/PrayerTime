20d3d9610c3cba4c621cc24d2338cc69a7f5f40a
asma <as_xx2@icloud.com>
2025-11-29 21:49:06 +0300

    Initial Commit

---
+++ PrayerTime/PrayerTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerTimeApp.swift
+//  PrayerTime
+//
+//  Created by asma  on 08/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct PrayerTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
